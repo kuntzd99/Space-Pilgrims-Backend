47083d4dfb387e946d7c91e6c03d1227122d4bec
Mark Kennedy <kennedymark@hotmail.com>
2022-04-15 11:21:46 -0400

    Changed the table names to lowercase

--- migrations/20220415144745-create-traveler.js
+++ migrations/20220415144745-create-traveler.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Travelers', {
+    await queryInterface.createTable('travelers', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,9 +28,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Travelers');
+    await queryInterface.dropTable('travelers')
   }
-};
+}
