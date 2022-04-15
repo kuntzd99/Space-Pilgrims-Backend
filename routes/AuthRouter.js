b5811c27453ed27a1d222ad55596abec9f87aa33
Chinwendu <chinwenduukoha@gmail.com>
2022-04-15 18:56:20 -0400

    Backend done from Auth side; Backend done from Pilgrim side

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -4,17 +4,17 @@ const middleware = require('../middleware')
 
 router.post('/login', controller.Login)
 router.post('/register', controller.Register)
-// router.post(
-//   '/update',
-//   middleware.stripToken,
-//   middleware.verifyToken,
-//   controller.UpdatePassword
-// )
-// router.get(
-//   '/session',
-//   middleware.stripToken,
-//   middleware.verifyToken,
-//   controller.CheckSession
-// )
+router.post(
+  '/update/:pilgrim_id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdatePassword
+)
+router.get(
+  '/session',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CheckSession
+)
 
 module.exports = router
