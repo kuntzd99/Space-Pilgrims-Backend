735366a50850c9091975c5226c11bbffb3471b54
Mark Kennedy <kennedymark@hotmail.com>
2022-04-15 12:10:42 -0400

    Added images to planet and traveler models.

--- migrations/20220415145002-create-planet.js
+++ migrations/20220415145002-create-planet.js
@@ -14,6 +14,9 @@ module.exports = {
       description: {
         type: Sequelize.STRING
       },
+      image: {
+        type: Sequelize.STRING
+      },
       population: {
         type: Sequelize.INTEGER
       },
--- models/planet.js
+++ models/planet.js
@@ -16,6 +16,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       name: DataTypes.STRING,
       description: DataTypes.STRING,
+      image: DataTypes.STRING,
       population: DataTypes.INTEGER
     },
     {
