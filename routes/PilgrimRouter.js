80f0b430215173cecc8b8526b266dd1c13f3137e
Daniel Kuntz <kuntzd99@gmail.com>
2022-04-16 14:01:56 -0500

    Forgot to save a file

--- routes/PilgrimRouter.js
+++ routes/PilgrimRouter.js
@@ -3,7 +3,6 @@ const controller = require('../controllers/PilgrimController')
 const middleware = require('../middleware')
 
 router.get('/:community_id', controller.GetPilgrimsByCommunity)
-
 router.put('/:pilgrim_id', controller.UpdatePilgrim)
 
 module.exports = router
