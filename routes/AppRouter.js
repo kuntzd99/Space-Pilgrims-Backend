f5700137bca2670baa96779992e99726e58b5fb7
Daniel Kuntz <kuntzd99@gmail.com>
2022-04-18 10:54:04 -0500

    Created controller functions to get and create comments

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -3,10 +3,12 @@ const AuthRouter = require('./AuthRouter')
 const PilgrimRouter = require('./PilgrimRouter')
 const PlanetRouter = require('./PlanetRouter')
 const CommunityRouter = require('./CommunityRouter')
+const CommentRouter = require('./CommentRouter')
 
 Router.use('/auth', AuthRouter)
 Router.use('/pilgrim', PilgrimRouter)
 Router.use('/planet', PlanetRouter)
 Router.use('/community', CommunityRouter)
+Router.use('/comment', CommentRouter)
 
 module.exports = Router
