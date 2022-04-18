55ca58f4cf1994305a360017f233507796450a1e
Chinwendu <chinwenduukoha@gmail.com>
2022-04-18 14:06:37 -0400

    changed data type for bio in pilgrims model

--- models/pilgrim.js
+++ models/pilgrim.js
@@ -43,7 +43,7 @@ module.exports = (sequelize, DataTypes) => {
         }
       },
       image: DataTypes.STRING,
-      bio: DataTypes.STRING,
+      bio: DataTypes.TEXT,
       admin: DataTypes.BOOLEAN
     },
     {
