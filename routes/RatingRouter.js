b822c94250d1ca645b89950f654def1c0d9928b5
Mark Kennedy <kennedymark@hotmail.com>
2022-04-20 14:47:24 -0400

    Created GetAverageRating

--- routes/RatingRouter.js
+++ routes/RatingRouter.js
@@ -2,5 +2,6 @@ const Router = require('express').Router()
 const controller = require('../controllers/RatingController')
 
 Router.post('/:planet_id/', controller.PostRating)
+Router.get('/:planet_id', controller.GetAverageRating)
 
 module.exports = Router
